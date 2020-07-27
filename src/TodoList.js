e8c94851984d48c03bde7fe34c37797d26e221d6
Hassan Muhashamy <hassan.muhashamy@gmail.com>
2020-07-27 08:27:11 +0300

    rendering static todos from state

---
+++ src/TodoList.js
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+import Todo from "./Todo";
+
+class TodoList extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      todos: [{ task: "Walk the dog" }, { task: "Feed the fish" }],
+    };
+  }
+
+  render() {
+    const todos = this.state.todos.map((todo) => {
+      return <Todo task={todo.task} />;
+    });
+    return (
+      <div>
+        <h1>TodoList</h1>
+        <ul>{todos}</ul>
+      </div>
+    );
+  }
+}
+
+export default TodoList;
